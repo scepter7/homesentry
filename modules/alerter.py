606f01bdc246d7ba5fade89a6c89bae02668a5a0
bastila <johnkaleida@protonmail.ch>
2018-04-21 02:26:28 +0300

    Minor updates

--- modules/alerter.py
+++ modules/alerter.py
@@ -3,6 +3,7 @@ import os
 
 
 class Alerter:
+    '''Create an image to be sent to Telegram. Allow cleaning it up if necessary.'''
     def __init__(self, basePath="./", ext=".jpg"):
         self.name = "{rand}{ext}".format(rand=str(uuid.uuid4()), ext=ext)
         self.path = "{base_path}/{image_name}".format(base_path=basePath, image_name=self.name)
