d001b39c7330de1a44647fea3259c97e98b68655
bastila <johnkaleida@protonmail.ch>
2018-04-29 06:15:50 +0300

    Add methods docstrings

--- server.py
+++ server.py
@@ -6,14 +6,18 @@ from md_manager import start_md
 
 
 class RequestHandler(BaseHTTPRequestHandler):
+    """Custom Request Handler for commands from Telegram Bot."""
 
     def do_GET(self):
+        """GET handler."""
+
         self.send_response(200)
         self.end_headers()
         return
-    
 
     def do_POST(self):
+        """POST handler. Process Telegram Bot commands."""
+
         content_len = int(self.headers.get("content-length"))
         post_body = self.rfile.read(content_len)
         data = json.loads(post_body)
