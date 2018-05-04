460583f74870157b2d46ebe6bf03f101534a7c23
bastila <johnkaleida@protonmail.ch>
2018-05-04 03:08:48 +0300

    Minor updates

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from http.server import HTTPServer
-from threading import Thread
-
 from server import RequestHandler
-from md_manager import start_md
 
 
 if __name__ == "__main__":
 
-    server = HTTPServer(('localhost', 8000), RequestHandler)
+    SERVER = HTTPServer(('localhost', 8000), RequestHandler)
     print("[INFO]Starting server at localhost:8000...")
-    server.serve_forever()
+    SERVER.serve_forever()
